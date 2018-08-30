d7a580e9387ad239c8d96dddc73a3e2c56788390
johnnyboldt <johnny.boldt@gmail.com>
2018-08-30 11:41:54 -0600

    Refactoring

--- src/containers/ItemInputContainer.tsx
+++ src/containers/ItemInputContainer.tsx
@@ -5,7 +5,7 @@ import { Item } from "../models/ItemsStore";
 export interface ItemInputContainerProps {
     itemList: any;
 }
-// const API = 'https://hn.algolia.com/api/v1/search?query=';
+ const API_URL = 'http://localhost:52395/api/values';
 
 const addItem = (name: string, value: number, category: string, itemList: any): void => {
   // todo: move this into method and add error checking. Add similiar call for delete
@@ -13,7 +13,7 @@ const addItem = (name: string, value: number, category: string, itemList: any): 
   formData.append('name', '123');
   formData.append('value', value.toString());
   formData.append('category', category);
-  fetch('http://localhost:51840/api/values', {
+  fetch(API_URL, {
     body: JSON.stringify({ name: 'test@test.com', value: 'Test123!', category: 'password' }),
    // headers: {'Content-Type':'application/json'},
    headers: {'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8'},
